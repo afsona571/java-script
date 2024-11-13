60b4c8f38ef5760c46721a17cb451dd8b8afc27c
afsona571 <afsonaa59@gmail.com>
2024-11-13 17:47:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// let firstName = prompt("Введите ваше имя:");
+// let lastName = prompt("Введите вашу фамилию:");
+// let age = prompt("Введите ваш возраст:");
+// let location = prompt("Где вы живете?");
+let lastName = prompt("Введите вашу фамилию:");
+let firstName = prompt("Введите ваше имя:");
+let age = prompt("Введите ваш возраст:");
+let loc = prompt("Где вы живете?");
+let rax = alert('спасибо')
+
+
+console.log("Имя: " + firstName);
+console.log("Фамилия: " + lastName);
+console.log("Возраст: " + age);
+console.log("Место проживания: " + loc);
+
